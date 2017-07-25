59059add8e84e58d0b5fe1e168d51a33e9cbb4d5
vendetta <osmer19@hotmail.com>
2017-07-24 23:11:58 -0500

    fix layout

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,7 +1,9 @@
-@extends('layouts.app)
+@extends('layouts.app')
 
 @section('content')
-    <h1>{{$post->title}}</h1>
-    <p>{{$post->content}}</p>
-    <small>{{$post->user->name}}</small>
+    <div class="content">
+        <h1>{{$post->title}}</h1>
+        <p>{{$post->content}}</p>
+        <small>{{$post->user->name}}</small>
+    </div>
 @endsection
